6c3d7ca83d137fda81d655641713ce53244f802c
Jean <jeanncavalcante@gmail.com>
2022-12-15 15:39:33 -0300

    signIn

---
+++ scripts/signIn.js
@@ -0,0 +1,32 @@
+let user = getUser();
+
+const form = document.querySelector("#form-login");
+
+form.addEventListener("submit", login);
+
+function login(e) {
+  e.preventDefault();
+
+  const email = e.target[0].value;
+  const password = e.target[1].value;
+
+  const userAlreadyExists = user.find(
+    (user) => user.email === email && user.password === password
+  );
+
+    let userLogado = {
+      id: userAlreadyExists.id,
+      name: userAlreadyExists.name,
+      email: userAlreadyExists.email,
+      photo: userAlreadyExists.photo,
+      token: Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2),
+    }
+
+  if (userAlreadyExists) {
+    setStudentLogado(userLogado);
+    window.location.href = "./pages/home.html";
+    console.log(getUserLogado());
+  } else {
+    alert("E-mail ou senha incorretos");
+  }
+}
